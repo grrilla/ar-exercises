f281efeec057ff96c72596f76a42601d0e191bd2
grrilla <mike.longauer@gmail.com>
2016-08-03 18:55:09 +0000

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue: $#{'%.2f' % Store.sum(:annual_revenue)}"
+puts "Average revenue: $#{'%.2f' % Store.average(:annual_revenue)}"
+puts "Count of Stores with revenue >$1M: #{Store.where('annual_revenue >= 1000000').count}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Michael", last_name: "Longauer", hourly_rate: 100)
+@store1.employees.create(first_name: "Shaunald", last_name: "McDonald", hourly_rate: 5)
+@store1.employees.create(first_name: "Billy", last_name: "Bee", hourly_rate: 60)
+@store2.employees.create(first_name: "Paul", last_name: "Abdul", hourly_rate: 10)
+@store2.employees.create(first_name: "Ashley", last_name: "Longauer", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
